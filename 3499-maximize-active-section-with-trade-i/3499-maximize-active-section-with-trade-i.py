26a27fe75d400fad7dffc45c908495394bb017f5
Abdul Moid Warsi <abdulmoid322@gmail.com>
2026-07-21 12:13:24 +0530

    Time: 727 ms (48.6%), Space: 20.7 MB (78.5%) - LeetHub

--- 3499-maximize-active-section-with-trade-i/3499-maximize-active-section-with-trade-i.py
+++ 3499-maximize-active-section-with-trade-i/3499-maximize-active-section-with-trade-i.py
@@ -5,20 +5,15 @@ class Solution:
         mx = 0
         i = 0
         n = len(s)
-
         while i < n:
             j = i
             while j < n and s[j] == s[i]:
                 j += 1
-
             cnt = j - i
-
             if s[i] == '1':
                 ans += cnt
             else:
                 mx = max(mx, pre + cnt)
                 pre = cnt
-
             i = j
-
         return ans + mx
