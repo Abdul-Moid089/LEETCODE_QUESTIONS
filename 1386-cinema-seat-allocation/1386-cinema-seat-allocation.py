0dc578a07d7930130db93424cd34022c8792c799
Abdul Moid Warsi <abdulmoid322@gmail.com>
2026-08-19 10:28:12 +0530

    Time: 18 ms (98.2%), Space: 22.6 MB (89.46%) - LeetHub

---
+++ 1386-cinema-seat-allocation/1386-cinema-seat-allocation.py
@@ -0,0 +1,22 @@
+from collections import defaultdict
+
+class Solution:
+    def maxNumberOfFamilies(self, n, reservedSeats):
+        rows = defaultdict(int)
+
+        for r, s in reservedSeats:
+            rows[r] |= 1 << (s - 1)
+
+        ans = (n - len(rows)) * 2
+
+        left = 0b0000011110
+        middle = 0b0001111000
+        right = 0b0111100000
+
+        for seats in rows.values():
+            if seats & left == 0 and seats & right == 0:
+                ans += 2
+            elif seats & left == 0 or seats & middle == 0 or seats & right == 0:
+                ans += 1
+
+        return ans
