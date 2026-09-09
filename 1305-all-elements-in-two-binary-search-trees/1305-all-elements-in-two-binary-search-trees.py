a4cbab63add3a2b5cc37054ae2af14355be670a8
Abdul Moid Warsi <abdulmoid322@gmail.com>
2026-09-09 10:07:59 +0530

    Time: 108 ms (12.6%), Space: 24 MB (9.71%) - LeetHub

--- 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.py
+++ 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.py
@@ -1,5 +1,9 @@
 class Solution:
-    def getAllElements(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> List[int]:
+    def getAllElements(
+        self,
+        root1: Optional[TreeNode],
+        root2: Optional[TreeNode]
+    ) -> List[int]:
 
         def inorder(root):
             if not root:
@@ -10,7 +14,34 @@ class Solution:
         l1 = inorder(root1)
         l2 = inorder(root2)
 
-        final_l = l1 + l2
-        final_l.sort()
+        m = len(l1)
+        n = len(l2)
+
+        final_l = [0] * (m + n)
+
+        i = 0
+        j = 0
+        k = 0
+
+        while i < m and j < n:
+
+            if l1[i] <= l2[j]:
+                final_l[k] = l1[i]
+                i += 1
+            else:
+                final_l[k] = l2[j]
+                j += 1
+
+            k += 1
+
+        while i < m:
+            final_l[k] = l1[i]
+            i += 1
+            k += 1
+
+        while j < n:
+            final_l[k] = l2[j]
+            j += 1
+            k += 1
 
         return final_l
