d6658966e70b676e6ac95073b30343d0a7fa4343
Abdul Moid Warsi <abdulmoid322@gmail.com>
2026-09-18 01:46:01 +0530

    Time: 0 ms (100%), Space: 20.3 MB (79.21%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.py
@@ -0,0 +1,22 @@
+class Solution:
+    def canFinish(self, numCourses, prerequisites):
+        graph = [[] for _ in range(numCourses)]
+        indegree = [0] * numCourses
+
+        for course, prereq in prerequisites:
+            graph[prereq].append(course)
+            indegree[course] += 1
+
+        queue = [i for i in range(numCourses) if indegree[i] == 0]
+        completed = 0
+
+        for course in queue:
+            completed += 1
+
+            for next_course in graph[course]:
+                indegree[next_course] -= 1
+
+                if indegree[next_course] == 0:
+                    queue.append(next_course)
+
+        return completed == numCourses
